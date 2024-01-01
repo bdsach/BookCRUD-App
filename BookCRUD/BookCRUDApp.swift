fab7a5e4411705810e1f30ea7b34e9041533a120
Bandit Silachai <mbandit001@yahoo.co.th>
2024-01-01 10:53:36 +0700

    Initial Commit

---
+++ BookCRUD/BookCRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCRUDApp.swift
+//  BookCRUD
+//
+//  Created by Bandit Silachai on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BookCRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
